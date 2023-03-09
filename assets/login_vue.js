0994a5fd485f6c6f74b86e373838e1017fc36e8d
vams-git <109448159+vams-git@users.noreply.github.com>
2023-03-09 16:20:13 +1030

    update

--- assets/login_vue.js
+++ assets/login_vue.js
@@ -10,6 +10,9 @@ var login_ = {
       loader: false,
       time: 60,
       timer: null,
+      login_attempt: 0,
+      login_time: 1,
+      login_timer: null,
     }
   },
   watch: {
@@ -36,7 +39,7 @@ var login_ = {
       this.logout = input;
       return this
     },
-    reload(){
+    reload() {
       window.location.replace(headUrl)
     },
     updateTokenUrl(input) { this.tokenUrl = input; return this },
@@ -49,6 +52,34 @@ var login_ = {
         })
       }
       else { window.location.replace(updateUrl(headUrl, param)) }
+    },
+    log_in() {
+      this.login_attempt++;
+      login.updateLoading(true)
+      var fetch_status = new Request(updateUrl(login_gas, param), {
+        redirect: "follow",
+        method: 'POST',
+        headers: { "Content-Type": "text/plain;charset=utf-8" },
+      });
+      fetch(fetch_status)
+        .then(function (response) { return response.json() })
+        .then(function (data) {
+          if (!data.status && data.text.tokenUrl !== undefined &&
+            this.login_attempt <= 5) { setInterval(this.log_in, 2000) }
+          else if (data.status) {
+            var access = JSON.parse(JSON.stringify(param));
+            access['expiration'] = data.text.expiresAt;
+            access['userid'] = data.text.usr_code;
+            param['userid'] = data.text.usr_code;
+            access['organization'] = data.text.uog_org;
+            param['organization'] = data.text.uog_org;
+            access['name'] = data.text.usr_desc;
+            param['name'] = data.text.usr_desc;
+            window.localStorage.setItem('access', JSON.stringify(access));
+            window.location.replace(updateUrl(headUrl, param))
+          }
+          else { window.location.replace(updateUrl(headUrl, param)) }
+        })
     },
     log_out() {
       this.loading = true;
@@ -75,7 +106,7 @@ var login_ = {
       clearInterval(this.timer)
     },
     start() {
-      this.timer = setInterval(this.decrementOrAlert, 1000) 
+      this.timer = setInterval(this.decrementOrAlert, 1000)
       return this
     }
   }
