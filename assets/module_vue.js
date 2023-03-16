38615a41bfcff55b988a574317fce6bdea9b4eb5
vams-git <109448159+vams-git@users.noreply.github.com>
2023-03-17 09:37:56 +1030

    update

--- assets/module_vue.js
+++ assets/module_vue.js
@@ -47,18 +47,33 @@ var user_ = {
 };
 
 var loader_ = {
-  data() { return { 'loader': false } },
+  data() {
+    return {
+      'loader': false,
+      'queue': []
+    }
+  },
   methods: {
+    addLoader(item) {
+      this.startLoader()
+      this.queue.push(item);
+    },
+    deleteLoader(item) {
+      this.queue = this.queue.filter(function (e) { return e !== item });
+      if (this.queue.length === 0) {
+        this.stopLoader()
+      }
+    },
     startLoader() {
       this.loader = true;
       document.getElementById('header').classList.add("d-none");
-      document.getElementById('unfinished').classList.add("d-none");
+      document.getElementById('open_dvr').classList.add("d-none");
       document.getElementById('loader').classList.remove("d-none");
     },
     stopLoader() {
       this.loader = false;
       document.getElementById('header').classList.remove("d-none");
-      document.getElementById('unfinished').classList.remove("d-none");
+      document.getElementById('open_dvr').classList.remove("d-none");
       document.getElementById('loader').classList.add("d-none");
     }
   }
@@ -91,11 +106,125 @@ var search_ = {
       this.filtered_list = get_equipment; return
     },
     process(data) {
-      console.log(data.obj_org);
-      console.log(data.obj_code);
-      loader.startLoader()
-      setInterval(function(){loader.stopLoader()}, 3000)
+      var state = loader;
+      var parameters = param;
+      var url = login_gas;
+      state.addLoader('addchecklist');
+      var app_data = this;
+      var reqParam = {
+        'process': 'add_checklist',
+        'tenant': parameters.tenant,
+        'email': parameters.email,
+        'sessionid': parameters.sessionid,
+        'userid': parameters.userid,
+        'eqorg': data.obj_org,
+        'eq': data.obj_code
+      };
+      var fetch_data = new Request(updateUrl(url, reqParam), {
+        redirect: "follow",
+        method: 'POST',
+        headers: { "Content-Type": "text/plain;charset=utf-8" },
+      });
+      fetch(fetch_data)
+        .then(function (response) { return response.json() })
+        .then(function (data) {
+          if (data.status) {
+            console.log(data);
+            window.location.reload();
+          }
+          else {
+            console.log(data)
+            alert.add({
+              text: data.text,
+              type: 'error'
+            });
+            state.stopLoader('search')
+          }
+        });
+    },
+    init(state, url, parameters, userdata) {
+      state.addLoader('search')
+      var app_data = this;
+      var reqParam = {
+        'process': 'get_equipment_details',
+        'tenant': parameters.tenant,
+        'email': parameters.email,
+        'sessionid': parameters.sessionid,
+        'userid': parameters.userid,
+        'org': parameters.org,
+        'query': ''
+      };
+      var fetch_data = new Request(updateUrl(url, reqParam), {
+        redirect: "follow",
+        method: 'POST',
+        headers: { "Content-Type": "text/plain;charset=utf-8" },
+      });
+      fetch(fetch_data)
+        .then(function (response) { return response.json() })
+        .then(function (data) {
+          if (data.status) {
+            app_data.list = data.text;
+            app_data.org_list = userdata.org_list;
+            app_data.org.push(userdata.org);
+            document.getElementById('new_dvr_btn').classList.remove('disabled');
+            state.stopLoader('search')
+          }
+          else {
+            console.log(data)
+            alert.add({
+              text: data.text,
+              type: 'error'
+            });
+            state.stopLoader('search')
+          }
+        });
+    }
+  }
+};
 
+var open_dvr_ = {
+  data() {
+    return {
+      'list': [],
+      'open': false
+    }
+  },
+  methods: {
+    getList() {
+      var get_list = this.list.map(function (e) {
+        return {
+          'ock_code': e['ock_code'],
+          'text': e['obj_desc'] + ' (' + e['obj_udfchar39'] + ')',
+          'date': e['trunc_ock_startdate'],
+          'status': e['ock_state']
+        }
+      });
+      return get_list
+    },
+    init(state, url, parameters) {
+      state.addLoader('open_dvr');
+      var app_data = this;
+      var reqParam = {
+        'process': 'get_open_dvr',
+        'tenant': parameters.tenant,
+        'email': parameters.email,
+        'sessionid': parameters.sessionid,
+        'userid': parameters.userid
+      };
+      var fetch_data = new Request(updateUrl(url, reqParam), {
+        redirect: "follow",
+        method: 'POST',
+        headers: { "Content-Type": "text/plain;charset=utf-8" },
+      });
+      fetch(fetch_data)
+        .then(function (response) { return response.json() })
+        .then(function (data) {
+          if (data.status) {
+            app_data.list = data.text;
+            app_data.open = true;
+            state.stopLoader('open_dvr')
+          }
+        });
     }
   }
 };
